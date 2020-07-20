41d921c612eb5435920bdaca1bf73d7efdc74136
msquee <msqueo@osc.edu>
2020-07-20 16:18:40 -0400

    Add daemon functionality
    * Adds cobra for configuration
    * Add -D command line flag to run as daemon

--- main.go
+++ main.go
@@ -1,13 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+	"github.com/sevlyar/go-daemon"
+	"github.com/spf13/cobra"
 )
+
+var (
+	runAsDaemon bool
+)
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&runAsDaemon, "daemon", "D", false, "run Go WebSockify as daemon")
+}
+
+func main() {
+	Execute()
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:  "go-websockify",
+	Long: `Starts a WebSocket server which facilitates a bidirectional communications channel. Endpoints are responsible for implementing their own transport layer, Go WebSockify's only job is to move buffers from point A to B.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if runAsDaemon {
+			cntxt := &daemon.Context{
+				PidFileName: "go-websockify.pid",
+				PidFilePerm: 0644,
+			}
+
+			daemon, err := cntxt.Reborn()
+			if err != nil {
+				log.Fatalf("Unable to start %s", err.Error())
+			}
+			if daemon != nil {
+				return
+			}
+			defer cntxt.Release()
+
+			log.Println("Starting server listening on port 127.0.0.1:8080")
+			daemonMessage := fmt.Sprintf("Running daemon under PID %d", os.Getpid())
+			log.Println(daemonMessage)
+		} else {
+			log.Println("Starting server listening on port 127.0.0.1:8080")
+		}
+		Start()
+	},
+}
 
 var (
 	upgrader = websocket.Upgrader{
@@ -19,11 +71,7 @@ var (
 	proxyServer *ProxyServer
 )
 
-func init() {
-	log.Println("Starting Go WebSockify")
-}
-
-func main() {
+func Start() {
 	router := mux.NewRouter()
 	router.HandleFunc("/ws", webSocketHandler)
 	http.Handle("/", router)
