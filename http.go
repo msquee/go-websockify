e4fa4d454219c563c59b5dec9b174a539b209464
msquee <msqueo@osc.edu>
2020-07-21 13:52:11 -0400

    Abstract HTTP server into its own module
    * Add timeouts,
    * Build our own Server struct

---
+++ http.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:    65536,
+		WriteBufferSize:   65536,
+		CheckOrigin:       authenticateOrigin,
+		EnableCompression: true,
+	}
+	proxyServer *ProxyServer
+)
+
+func StartHTTP() {
+	server := &http.Server{
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       60 * time.Second,
+		Addr:              "localhost:8080",
+	}
+	router := mux.NewRouter()
+	router.HandleFunc("/ws", webSocketHandler)
+	http.Handle("/", router)
+	log.Fatal(server.ListenAndServe())
+}
+
+/*
+webSocketHandler handles an incoming HTTP upgrade
+request for a WebSocket connection while establishing a
+bidirectional stream to a proxied TCP resource.
+*/
+func webSocketHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("New WebSocket Connection from %s", r.RemoteAddr)
+	log.Println("Attempting to upgrade WebSocket connection")
+
+	wsConn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println("Failed to upgrade websocket request: ", err)
+		return
+	}
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", string("localhost:1000"))
+	if err != nil {
+		message := "Failed to resolve destination: " + err.Error()
+		log.Println(message)
+		_ = wsConn.WriteMessage(websocket.CloseMessage, []byte(message))
+		return
+	}
+
+	proxyServer = NewWebSocketProxy(wsConn, tcpAddr)
+	err = proxyServer.Dial()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	go proxyServer.Start()
+}
--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
-	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 	"github.com/sevlyar/go-daemon"
 	"github.com/spf13/cobra"
 )
@@ -45,6 +41,7 @@ var rootCmd = &cobra.Command{
 			daemon, err := cntxt.Reborn()
 			if err != nil {
 				log.Fatalf("Unable to start %s", err.Error())
+				return
 			}
 			if daemon != nil {
 				return
@@ -57,56 +54,6 @@ var rootCmd = &cobra.Command{
 		} else {
 			log.Println("Starting server listening on port 127.0.0.1:8080")
 		}
-		Start()
+		StartHTTP()
 	},
-}
-
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:    65536,
-		WriteBufferSize:   65536,
-		CheckOrigin:       authenticateOrigin,
-		EnableCompression: true,
-	}
-	proxyServer *ProxyServer
-)
-
-func Start() {
-	router := mux.NewRouter()
-	router.HandleFunc("/ws", webSocketHandler)
-	http.Handle("/", router)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-}
-
-/*
-webSocketHandler handles an incoming HTTP upgrade
-request for a WebSocket connection while establishing a
-bidirectional stream to a proxied TCP resource.
-*/
-func webSocketHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("New WebSocket Connection from %s", r.RemoteAddr)
-	log.Println("Attempting to upgrade WebSocket connection")
-
-	wsConn, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		log.Println("Failed to upgrade websocket request: ", err)
-		return
-	}
-
-	tcpAddr, err := net.ResolveTCPAddr("tcp", string("localhost:1000"))
-	if err != nil {
-		message := "Failed to resolve destination: " + err.Error()
-		log.Println(message)
-		_ = wsConn.WriteMessage(websocket.CloseMessage, []byte(message))
-		return
-	}
-
-	proxyServer = NewWebSocketProxy(wsConn, tcpAddr)
-	err = proxyServer.Dial()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	go proxyServer.Start()
 }
